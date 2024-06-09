5e3faa77d6a6779a082aaa49a96b2706e8b643f3
Van Chi Hieu <vanchihieu3@gmail.com>
2024-06-09 10:12:58 +0700

    'Hello world'

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
