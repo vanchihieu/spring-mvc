2223b758beb9ef3139c5392d4e65f09485ea8f8c
Van Chi Hieu <vanchihieu3@gmail.com>
2024-06-12 15:15:52 +0700

    Design Giao Diện Admin

--- src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
+++ src/main/java/vn/hoidanit/laptopshop/config/WebMvcConfig.java
@@ -29,5 +29,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
+
     }
 }
