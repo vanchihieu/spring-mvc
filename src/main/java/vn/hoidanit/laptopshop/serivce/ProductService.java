4230dde5a543f064a61b5d34ec731b5fd3f43d9e
Van Chi Hieu <vanchihieu3@gmail.com>
2024-06-18 22:57:14 +0700

    Design giao diện chi tiết giỏ hàng

--- src/main/java/vn/hoidanit/laptopshop/serivce/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/serivce/ProductService.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.serivce;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 import vn.hoidanit.laptopshop.domain.Cart;
 import vn.hoidanit.laptopshop.domain.CartDetail;
@@ -43,7 +44,7 @@ public class ProductService {
         this.productRepository.deleteById(id);
     }
 
-    public void handleAddProductToCart(String email, long productId) {
+    public void handleAddProductToCart(String email, long productId, HttpSession session) {
         User user = this.userService.getUserByEmail(email);
         if (user != null) {
             // check user đã có Cart chưa ? nếu chưa -> tạo mới
@@ -53,7 +54,7 @@ public class ProductService {
                 // tạo mới cart
                 Cart otherCart = new Cart();
                 otherCart.setUser(user);
-                otherCart.setSum(1);
+                otherCart.setSum(0);
 
                 cart = this.cartRepository.save(otherCart);
             }
@@ -61,15 +62,29 @@ public class ProductService {
             // save cart_detail
             // tìm product by id
             Optional<Product> productOptional = this.productRepository.findById(productId);
-            if (productOptional.isPresent()) {
+            if (productOptional.isPresent()) { // nếu product tồn tại
                 Product realProduct = productOptional.get();
 
-                CartDetail cd = new CartDetail();
-                cd.setCart(cart);
-                cd.setProduct(realProduct);
-                cd.setPrice(realProduct.getPrice());
-                cd.setQuantity(1);
-                this.cartDetailRepository.save(cd);
+                // check sản phẩm đã từng được thêm vào giỏ hàng trước đây chưa ?
+                CartDetail oldDetail = this.cartDetailRepository.findByCartAndProduct(cart, realProduct);
+
+                if(oldDetail == null){
+                    CartDetail cd = new CartDetail();
+                    cd.setCart(cart);
+                    cd.setProduct(realProduct);
+                    cd.setPrice(realProduct.getPrice());
+                    cd.setQuantity(1);
+                    this.cartDetailRepository.save(cd);
+
+                    // update sum
+                    int s = cart.getSum() + 1;
+                    cart.setSum(s);
+                    this.cartRepository.save(cart);
+                    session.setAttribute("sum", s);
+                } else {
+                    oldDetail.setQuantity(oldDetail.getQuantity() + 1);
+                    this.cartDetailRepository.save(oldDetail);
+                }
             }
 
         }
